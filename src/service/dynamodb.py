7cb02e2df3d2565d4285e6955bc6768f39bea3ad
Juan Manuel Ruiz Fernández <jruizferprofesional@gmail.com>
2021-08-15 14:08:42 +0200

    [services] Add dynamodb service
    Relates to SGCC-4

---
+++ src/service/dynamodb.py
@@ -0,0 +1,26 @@
+import json
+
+from decimal import Decimal
+
+from src.helper.boto import BotoResourceHelper
+
+
+class DynamoDBService(BotoResourceHelper):
+    def __init__(self, table_name: str):
+        super().__init__("dynamodb")
+        self.__table = self.resource.Table(table_name)
+
+    def find(self, id_name: str, item_id: str) -> dict:
+        try:
+            result = self.__table.get_item(Key={id_name: item_id})
+            return result["Item"] if "Item" in result else None
+        except Exception as e:
+            raise Exception(e)
+
+    def save(self, item: dict) -> dict:
+        try:
+            dynamodb_record = json.loads(json.dumps(item), parse_float=Decimal)
+            response = self.__table.put_item(Item=dynamodb_record)
+            return response
+        except Exception as e:
+            raise Exception(e)
